b2054fd4b694cadfaa853890755865f507e90970
datikken <tikken23@gmail.com>
2021-04-05 01:31:12 +0300

    basic assertions

--- PracticeTest.php
+++ PracticeTest.php
@@ -13,4 +13,35 @@ class PracticeTest extends TestCase
         $greeting = 'Hello World';
         $this->assertEquals('Hello World', $greeting);
     }
+
+    /** @test */
+    public function test_value_is_the_same()
+    {
+        $val = 0;
+        $this->assertSame(0, $val);
+    }
+
+    /** @test */
+    public function test_assert_contains()
+    {
+        $names = ['Taylor', 'Shawn', 'Dayle'];
+        $this->assertContains('Dayle', $names);
+    }
+
+    /** @test */
+    public function test_array_has_key()
+    {
+        $fam = [
+            'parents' => 'Joe',
+            'children' => ['Timmy', 'Suzy']
+        ];
+
+        $this->assertArrayHasKey('parents', $fam);
+    }
+
+    public function test_variable_type()
+    {
+        $age = 25;
+        $this->assertIsInt($age);
+    }
 }
